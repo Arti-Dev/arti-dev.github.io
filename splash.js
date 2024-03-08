9fa0e591d310714610e4e7bb89121cfc3a0527b9
Arti-Dev <boomcreeper296@gmail.com>
2024-03-08 15:05:12 -0500

    Add splash text

---
+++ splash.js
@@ -0,0 +1,20 @@
+const possibleSplash =
+    ["why hello there.", "please solve a captcha before clicking this text!", "look behind you", "6", "pit",
+    "spin the t", "0 is a natural number!", "i feel like.................................skyblock",
+    "member of FYPit", "there's a pocket in my knife",
+    "how get axe", "figuring out what to do!"]
+
+function changeText() {
+    const text = document.getElementById("splash")
+    let randomInt = getRandomInteger(0, possibleSplash.length)
+    if (possibleSplash[randomInt] === text.innerHTML) {
+        randomInt += 1
+        randomInt %= possibleSplash.length;
+    }
+    text.innerHTML = possibleSplash[randomInt]
+}
+
+// taken from https://www.w3schools.com/js/js_random.asp
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+}
