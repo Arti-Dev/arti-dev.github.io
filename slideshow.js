e12adb3650b04a64d592c4d312302d5ab43cbedb
Aidan Liu <boomcreeper296@gmail.com>
2026-05-05 17:55:09 -0400

    Finish!

--- slideshow.js
+++ slideshow.js
@@ -1,19 +1,45 @@
-let slideIndex = 0;
+let slideIndex = 1;
+let autoplay = true;
+const timeout = 5000
 
 function nextSlide() {
-    showSlide(slideIndex += 1)
+    showSlide(slideIndex + 1)
+    // user click, stop autoplay
+    autoplay = false
 }
 
 function prevSlide() {
-    showSlide(slideIndex -= 1)
+    showSlide(slideIndex - 1)
+    autoplay = false
 }
 
 function showSlide(n) {
     let slides = document.getElementsByClassName("slide")
-    if (n >= slides.length) slideIndex = 0;
-    else if (n < 0) slideIndex = slides.length - 1;
+    slideIndex = n
+    if (slideIndex >= slides.length) slideIndex = 0;
+    else if (slideIndex < 0) slideIndex = slides.length - 1;
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     slides[slideIndex].style.display = "block"
 }
+
+function randomSlide() {
+    let slides = document.getElementsByClassName("slide")
+    console.log(slides.length)
+    showSlide(Math.floor(Math.random() * slides.length));
+}
+
+function startAutoplay() {
+    randomSlide()
+    setTimeout(autoNextSlide, timeout)
+}
+
+function autoNextSlide() {
+    if (autoplay) {
+        showSlide(slideIndex + 1)
+        setTimeout(autoNextSlide, timeout)
+    }
+}
+
+startAutoplay()
