726da54fcea3969d6789f7f881e891d213093c55
Aidan Liu <boomcreeper296@gmail.com>
2026-05-05 17:13:33 -0400

    Initial slideshow

---
+++ slideshow.js
@@ -0,0 +1,19 @@
+let slideIndex = 0;
+
+function nextSlide() {
+    showSlide(slideIndex += 1)
+}
+
+function prevSlide() {
+    showSlide(slideIndex -= 1)
+}
+
+function showSlide(n) {
+    let slides = document.getElementsByClassName("slide")
+    if (n >= slides.length) slideIndex = 0;
+    else if (n < 0) slideIndex = slides.length - 1;
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex].style.display = "block"
+}
